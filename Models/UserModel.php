f3299b9891dc9c4ec33f0c881442f6bf4c92213e
Ragnarohf <mathieuthoumire@hotmail.fr>
2021-04-26 11:10:07 +0200

    m

--- Models/UserModel.php
+++ Models/UserModel.php
@@ -1,2 +1,10 @@
 <?php 
 namespace App\Models;
+class UserModel{
+    private $_id_user;
+    private $_name;
+    private $_email;
+    private $_pwd;
+    private $_pwd2;
+    private $_avatar;
+}
