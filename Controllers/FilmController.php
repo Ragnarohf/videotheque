68b41f38c0901fbb499d611f8cb1c27b588154c7
Ragnarohf <mathieuthoumire@hotmail.fr>
2021-04-27 11:46:46 +0200

    mise en place des films

---
+++ Controllers/FilmController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Controllers;
+
+use App\Models\UserModel;
+
+require_once("../../vendor/autoload.php");
+
+use Gumlet\ImageResize;
+
+class FilmController
+{
+}
---
+++ Models/FilmModel.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace App\Models;
+
+use PDO;
+use App\Models\Model;
+
+class FilmModel extends Model
+{
+}
